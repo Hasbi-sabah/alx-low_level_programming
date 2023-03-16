a3dd6bd2940130ed2543ca11df5856a0f6d11028
H. Sabah <sabahhasbi00@gmail.com>
2023-03-16 06:45:27 -0700

    mod 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Description: idk
+  *
+  *Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
