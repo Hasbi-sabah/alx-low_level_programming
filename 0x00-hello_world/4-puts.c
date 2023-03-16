b53d3e0dc365762055ee5c265823c898115a38e0
H. Sabah <sabahhasbi00@gmail.com>
2023-03-16 01:51:24 -0700

    betty mod in 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(){
-
+int main(void)
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return(0);
 
+	puts(str);
+	return (0);
 }
