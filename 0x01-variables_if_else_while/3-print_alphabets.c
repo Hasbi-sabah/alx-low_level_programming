78e4df4cb24b823f9a4627a64a34a8d37f9be35a
H. Sabah <sabahhasbi00@gmail.com>
2023-03-17 02:11:02 -0700

    added file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+	for (chr = 'A'; chr <= 'Z'; chr++)
+                putchar(chr);
+	putchar('\n');
+	return (0);
+}
