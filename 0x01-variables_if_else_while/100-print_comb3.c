be0ca7ab598cefffd4f114754ac3a3c57d04f2d8
H. Sabah <sabahhasbi00@gmail.com>
2023-03-17 04:07:19 -0700

    added file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0, n2 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 9; n2++)
+		{
+			putchar('0' + n1);
+			putchar('0' + n2);
+			if (n1 != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
