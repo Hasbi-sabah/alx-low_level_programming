2779552101bf643f5c20754e5b43ee6349df3e17
H. Sabah <sabahhasbi00@gmail.com>
2023-03-17 03:07:14 -0700

    added file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
