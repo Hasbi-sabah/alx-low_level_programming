fe11f3240e74623bb9b99db70ff119aa15709e6c
H. Sabah <sabahhasbi00@gmail.com>
2023-03-21 03:05:06 -0700

    added readme and file 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+	
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	putchar('\n');
+	return 0;
+}
