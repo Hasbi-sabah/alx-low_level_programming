e0172fa157d50fb8ded4a805897a6068fa3b15bf
H. Sabah <sabahhasbi00@gmail.com>
2023-03-21 08:49:02 -0700

    added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * Return: result of addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	times_table();
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
 	return (0);
 }
