0c3b9587670b4f80cd893771cc25ccc5091fda60
H. Sabah <sabahhasbi00@gmail.com>
2023-03-22 02:22:31 -0700

    added readme and file 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * positive_or_negative - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
