e9fa1150923cedc459836a342bccc8ed2469fcec
H. Sabah <sabahhasbi00@gmail.com>
2023-03-22 04:03:51 -0700

    added =

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && (b > c || b == 0 || c == 0))
+	if (a >= b && (a >= c || b == 0 || c == 0))
 	{
 		largest = a;
 	}
-	else if (b > a && (a > c || a == 0 || c == 0))
+	else if (b >= a && (b >= c || a == 0 || c == 0))
 	{
 		largest = b;
 	}
-
-	else if (c != 0)
+	else
 	{
 		largest = c;
 	}
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
+	a = 0;
+	b = 0;
+	c = -3;
 
 	largest = largest_number(a, b, c);
 	printf("%d is the largest number\n", largest);
