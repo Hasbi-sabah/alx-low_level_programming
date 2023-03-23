ccd7c1ed17b24c90746095068ec1a4f04ff11bf6
H. Sabah <sabahhasbi00@gmail.com>
2023-03-23 04:21:16 -0700

    added file 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 4 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 4 == 0)
+				printf("Buzz");
+		}
+		else
+			printf("%i", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
