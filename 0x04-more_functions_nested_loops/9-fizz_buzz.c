918d9b29d185919d09aae99334a4e7a9e98fda8b
H. Sabah <sabahhasbi00@gmail.com>
2023-03-23 04:23:13 -0700

    replaced 4 with 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 || i % 4 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
 				printf("Fizz");
-			if (i % 4 == 0)
+			if (i % 5 == 0)
 				printf("Buzz");
 		}
 		else
