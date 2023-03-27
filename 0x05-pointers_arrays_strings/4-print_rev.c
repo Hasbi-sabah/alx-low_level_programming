d7628beeabd30fffae88d983f2856062adc26fce
H. Sabah <sabahhasbi00@gmail.com>
2023-03-27 07:39:29 -0700

    change of i

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *str)
 {
 	int i;
 
-	for (i = 1; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
-	for (; i >= 0; i--)
+	for (i -= 1; i >= 0; i--)
 		_putchar(str[i]);
 	_putchar('\n');
 }
