f62941790f324435503adfd14ed5d979c7b0ab8b
H. Sabah <sabahhasbi00@gmail.com>
2023-03-27 09:37:04 -0700

    better work

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	for (j = i / 2; j <= i; j++)
+	for (j = (i + 1)/ 2; str[j] != '\0'; j++)
 		_putchar(str[j]);
 	_putchar('\n');
 }
