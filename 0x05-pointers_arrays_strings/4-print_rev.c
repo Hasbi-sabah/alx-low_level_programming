2deb20ff741a12851749ed488efcac04fb978955
H. Sabah <sabahhasbi00@gmail.com>
2023-03-27 07:35:33 -0700

    added file 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ *@str: string
+ */
+
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (; i >= 0; i--)
+		_putchar(str[i]);
+	_putchar('\n');
+}
