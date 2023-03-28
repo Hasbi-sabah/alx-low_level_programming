5aaa2a6f4b32ae40ecaeba082b7cc0126cbe4a6e
H. Sabah <sabahhasbi00@gmail.com>
2023-03-28 05:41:48 -0700

    added file 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <limits.h>
+#include <time.h>
 #include <stdlib.h>
 
 /**
@@ -10,6 +10,22 @@
 
 int main(void)
 {
-	printf("%d\n", rand() % INT_MAX);
+	int sum, i, n;
+	char password[100];
+
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 	return (0);
 }
