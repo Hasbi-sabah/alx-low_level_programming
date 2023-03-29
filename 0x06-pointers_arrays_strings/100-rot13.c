e5c198c8599c7f1c19699400ddde2c43d774d081
H. Sabah <sabahhasbi00@gmail.com>
2023-03-29 07:34:30 -0700

    added file 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13.
+ *
+ *@s: string to be changed
+ * Return: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char og[] = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN",
+	     rot[] = "nmreglhvbcdfqstuwxyzjkpioaNMREGLHVBCDFQSTUWXYZJKPIOA";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; og[j] != '\0'; j++)
+		{
+			if (s[i] == og[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
