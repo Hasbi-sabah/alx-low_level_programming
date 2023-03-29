d1ffa9e179dd72b931afa209494e56ae8d3bad29
H. Sabah <sabahhasbi00@gmail.com>
2023-03-29 08:40:38 -0700

    changed 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int sum1, sum2, i;
-
-	for (sum1 = 0, i = 0; s1[i] != '\0'; i++)
-		sum1 = (sum1 * 10) + s1[i] - '0';
-	for (sum2 = 0, i = 0; s2[i] != '\0'; i++)
-		sum2 = (sum2 * 10) + s2[i] - '0';
-	return (sum1 - sum2);
+	for (; s1 == s2; s1++, s2++)
+	{
+		if (*s1 == '\0')
+			return (0);
+	}
+	return (s1 - s2);
 }
