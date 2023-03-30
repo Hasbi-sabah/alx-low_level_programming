e0fb4b166a1bf96fbb5b155b79d605e070247f9b
H. Sabah <sabahhasbi00@gmail.com>
2023-03-30 03:13:19 -0700

    more mods

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,23 +13,21 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2;
+	int l1, l2, k;
 	int biggest, sum, leftover;
 
 	for (l1 = 0; n1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; n2[l2] != '\0'; l2++)
 		;
-	l1--;
-	l2--;
-	size_r--;
+	l1--, l2--, size_r--;
 	if (l1 >= l2)
 		biggest = l1;
 	else
 		biggest = l2;
 	if (biggest + 2 <= size_r)
 	{
-		for (leftover = 0; biggest >= 0; l1--, l2--, biggest--)
+		for (k = 0, leftover = 0; biggest >= 0; k++, l1--, l2--, biggest--)
 		{
 			sum = 0;
 			if (l1 >= 0 && l2 >= 0)
@@ -49,6 +47,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		if (leftover == 1)
 			*(r + 0) = leftover + '0';
+		for (k += 1; k <= size_r; k++)
+			*(r + k) = '\0';
 		return (r);
 	}
 	else
