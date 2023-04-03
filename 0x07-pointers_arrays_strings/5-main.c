d1e3d0fbdaee529b2dfed0b7ccdfc694f6e0d070
H. Sabah <sabahhasbi00@gmail.com>
2023-04-03 05:09:25 -0700

    mods in 5

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char *s = "hello, world";
-	char *f = "world";
+	char *f = "s";
 	char *t;
 
 	t = _strstr(s, f);
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,27 +10,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, n;
+	int i, j;
 
 	if (*haystack == '\0' && *needle == '\0')
-		return (0);
-	for (i = 0, n = 0; needle[i] != '\0'; i++)
+		return (NULL);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if ((haystack[j] == needle[i]
-					&& n == 0)
-					|| (haystack[j - 1] == needle[i - 1]
-						&& haystack[j] == needle[i]))
+			if (needle[j] != haystack[i + j])
 				break;
 		}
-		if (haystack[j] != '\0' && n == 0)
-			n = j;
-		else if (haystack[j] == '\0')
-			n = 0;
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 	}
-	if (n != 0)
-		return (&haystack[n]);
-	else
-		return (0);
+	return (NULL);
 }
