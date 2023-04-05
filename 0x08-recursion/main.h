10e0ea8a0fe0c3fb399d19c5994842c52ce58d4a
H. Sabah <sabahhasbi00@gmail.com>
2023-04-05 04:14:59 -0700

    added file 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square_root(int n, int i);
+int is_prime_number(int n);
+int prime(int n, int i);
+
 #endif /* MAIN_H */
