97705a233905d0e5a8d8dee4afdb08d7c8e1d7a0
H. Sabah <sabahhasbi00@gmail.com>
2023-04-07 01:45:28 -0700

    added file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	arg1 = strtol(argv[1], NULL, 10);
+	arg2 = strtol(argv[2], NULL, 10);
+	printf("%i\n", arg1 * arg2);
+	return (0);
+}
