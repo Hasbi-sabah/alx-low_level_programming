6e8137586159feebd70649e929fc39fad3137689
H. Sabah <sabahhasbi00@gmail.com>
2023-04-11 03:54:33 -0700

    seg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,36 +14,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int i, len1, len2;
+	int i = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-	{
-		len1 = 0;
-		for (len2 = 0; s2[len2] != '\0'; len2++)
-			;
-		p = (char *)malloc(sizeof(char) * (len2 + 1));
-	}
-	else if (s2 == NULL)
-	{
-		len2 = 0;
-		for (len1 = 0; s1[len1] != '\0'; len1++)
-			;
-		p = (char *)malloc(sizeof(char) * (len1 + 1));
-	}
-	else
-	{
-		for (len1 = 0; s1[len1] != '\0'; len1++)
-			;
-		for (len2 = 0; s2[len2] != '\0'; len2++)
-			;
-		p = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-	}
+	for (len1 = 0; s1[len1] != '\0' && s1 != NULL; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0' && s2 != NULL; len2++)
+		;
+	p = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0; i < len1 && s1 != NULL; i++)
 		p[i] = s1[i];
-	for (i = 0; i < len2; i++)
+	for (i = 0; i < len1 + len2 && s2 != NULL; i++)
 		p[len1 + i] = s2[i];
+	p[len1 + len2] = '\0';
 	return (p);
 	free(p);
 }
