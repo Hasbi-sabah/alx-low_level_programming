8fd0689ef903bffec212f3c22395c65cbb91f864
H. Sabah <sabahhasbi00@gmail.com>
2023-04-11 02:29:53 -0700

    mod in 0(if)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	char *p = (char *)malloc(sizeof(char) * size);
 	unsigned int i;
 
-	if (size == 0)
+	if (size == 0 || p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
