1508c545a268fb660eb95a0a56aff11a1c3bb740
H. Sabah <sabahhasbi00@gmail.com>
2023-04-11 16:31:23 -0700

    e

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,9 +17,11 @@ char **strtow(char *str)
 
 	for (i = 0, words = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' && i != 0 && str[i - 1] != ' ')
+		if (str[i] == ' ' && (i != 0 || str[i - 1] != ' '))
 			words++;
 	}
+	if (str[i - 1] == ' ')
+		words++;
 	p = malloc(sizeof(char *) * (words + 1));
 	if (p == NULL || words == 0)
 		return (NULL);
@@ -27,7 +29,7 @@ char **strtow(char *str)
 	{
 		if (str[i] != ' ')
 		{
-			for (len = 0; str[len] != ' ' && str[len] != '\0'; len++)
+			for (len = 0; str[i + len] != ' ' && str[i + len] != '\0'; len++)
 				;
 			p[k] = malloc(sizeof(char) * (len + 2));
 			if (p[k] == NULL)
@@ -37,7 +39,7 @@ char **strtow(char *str)
 				free(p);
 				return (NULL);
 			}
-			for (j = 0; str[i] != ' ' && str[i] != '\0'; j++, i++)
+			for (j = 0; j < len; j++, i++)
 				p[k][j] = str[i];
 			p[k][j] = '\0';
 			if (str[i] != '\0')
