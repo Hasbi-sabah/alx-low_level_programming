bc3147f6daa3ddd6e150ba8946c0c14e31d0b217
H. Sabah <sabahhasbi00@gmail.com>
2023-04-11 02:46:24 -0700

    added len + 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	p = (char *)malloc(sizeof(char) * len);
+	p = (char *)malloc(sizeof(char) * (len + 1));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
