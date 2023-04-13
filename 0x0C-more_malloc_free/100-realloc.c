1fa82ff657c7149afa2be71ac42de79be74cd6ea
H. Sabah <sabahhasbi00@gmail.com>
2023-04-13 04:33:10 -0700

    try

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; x_ptr != NULL && x_ptr[i] != '\0' && i < new_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 		p[i] = x_ptr[i];
 	free(ptr);
 	return (p);
