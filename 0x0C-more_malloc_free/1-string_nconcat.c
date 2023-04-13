21ea7c2d48e39dbb6d35d3c8f52f3df441b6514f
H. Sabah <sabahhasbi00@gmail.com>
2023-04-13 02:20:07 -0700

    fixed seg fault

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *p;
 
+	if (s2 == NULL || s2[0] == '\0')
+		n = 0;
 	p = malloc(sizeof(s1) + n + 1);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != 0 && s1 != NULL; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		p[i] = s1[i];
-	for (j = 0; s2[j] != 0 && s2 != NULL && j <= n; j++)
+	for (j = 0; s2 != NULL && j < n; j++)
 		p[i + j] = s2[j];
-	p[i + j] = '\0';
 	return (p);
 }
