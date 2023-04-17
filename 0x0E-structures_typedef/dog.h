9407fd5fb6bc6245c6c5bbe7dbca6f595669ba60
H. Sabah <sabahhasbi00@gmail.com>
2023-04-17 04:39:27 -0700

    added file 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+/**
+ * struct dog - dog info
+ *
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
