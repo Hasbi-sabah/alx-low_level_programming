85984be9115447f54bb6469eb81a2bd38a828925
H. Sabah <sabahhasbi00@gmail.com>
2023-04-17 05:06:44 -0700

    added if to malloc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -11,10 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (name == NULL)
-		name = "";
-	if (owner == NULL)
-		owner = "";
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
