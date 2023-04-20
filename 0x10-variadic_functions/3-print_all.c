b63448a24f4ece47bb718673fc57ef889d8e976a
H. Sabah <sabahhasbi00@gmail.com>
2023-04-20 02:14:51 -0700

    added file 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list pars;
+	char_list printer[] = {
+		{'c', c_print},
+		{'i', i_print},
+		{'f', f_print},
+		{'s', s_print}
+	};
+	int i, j;
+
+	i = 0;
+	va_start(pars, format);
+	while (format[i])
+	{
+		j = 0;
+		while (printer[j].chars)
+		{
+			if (format[i] == printer[j].chars)
+			{
+				printer[j].f(pars);
+				if (format[i + 1])
+					printf(", ");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(pars);
+	printf("\n");
+}
+
+/**
+ * c_print - prints c
+ * @pars: va list
+ */
+
+void c_print(va_list pars)
+{
+	printf("%c", va_arg(pars, int));
+}
+
+/**
+ * i_print - prints i
+ * @pars: va list
+ */
+
+void i_print(va_list pars)
+{
+	printf("%i", va_arg(pars, int));
+}
+
+/**
+ * f_print - prints f
+ * @pars: va list
+ */
+
+void f_print(va_list pars)
+{
+	printf("%f", va_arg(pars, double));
+}
+
+/**
+ * s_print - prints s
+ * @pars: va list
+ */
+
+void s_print(va_list pars)
+{
+	printf("%s", va_arg(pars, char *));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,25 @@
 #define VAR_FUN_H
 
 #include <stdarg.h>
+
+/**
+ * struct list - list of chars
+ * @chars: characters to look for
+ * @f: function to be executed
+ */
+struct list
+{
+	char chars;
+	void (*f)(va_list);
+};
+typedef struct list char_list;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void c_print(va_list pars);
+void i_print(va_list pars);
+void f_print(va_list pars);
+void s_print(va_list pars);
 
 #endif
