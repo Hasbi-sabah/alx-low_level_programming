95b152346b29bc995d0a60d6dc88dcd1ab3c590a
H. Sabah <sabahhasbi00@gmail.com>
2023-04-27 03:34:57 -0700

    betty :)

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,16 @@
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list
+ *
+ * Return: number of nodes
+ */
+
 size_t print_list(const list_t *h)
 {
 	int i;
+
 	for (i = 0; h; h = h->next, i++)
 	{
 		if (!h->str)
