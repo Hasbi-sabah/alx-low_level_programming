3bc05c29b7ae011a58bfb9117090ae8367e4dae8
H. Sabah <sabahhasbi00@gmail.com>
2023-04-27 12:14:43 -0700

    changed 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-	global main
+          global    main
+          extern    printf
 main:
-	mov edx,len
-	mov ecx, msg
-	mov ebx,1
-	mov eax,4
-	int 0x80
-
-	mov eax,1
-	int 0x80
-
-section .data
-msg db 'Hello, Holberton', 0xa
-len equ $ - msg
+	  mov   edi, hello
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+hello: db `Hello, Holberton\n`,0
