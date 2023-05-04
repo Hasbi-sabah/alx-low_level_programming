17b313a5e4d3ee8f715b3972ebf6d642923e33a7
H. Sabah <sabahhasbi00@gmail.com>
2023-05-04 03:54:12 -0700

    ulong_max

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,12 +10,13 @@ void print_binary(unsigned long int n)
 	unsigned long int flip;
 	int i;
 
-	if (!n)
+	if (n == 0)
 		_putchar('0');
-	for (flip = 0, i = 0; n > 0; i++, n >>= 1, flip <<= 1)
+	for (flip = 0, i = 0; n > 0; i++, n >>= 1)
+	{
+		flip <<= 1;
 		flip |= (1 & n);
-	for (flip >>= 1; i; flip >>= 1, i--)
+	}
+	for (; i; flip >>= 1, i--)
 		_putchar((1 & flip) + '0');
 }
-
-
