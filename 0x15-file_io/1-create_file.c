67bc6b5fea72c6a03480b5b89a7c76ac3a07672c
H. Sabah <sabahhasbi00@gmail.com>
2023-05-08 02:59:13 -0700

    added file 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int ret;
+	ssize_t n;
+
+	if (!filename)
+		return (-1);
+	ret = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (ret == -1)
+		return (-1);
+	n = write(ret, text_content, strlen(text_content));
+	if (n == -1)
+		return (-1);
+	close(ret);
+	return (1);
+}
