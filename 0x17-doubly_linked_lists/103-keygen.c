32fad2bbbd89da54ea83b3bdf09487daf0c37580
H. Sabah <sabahhasbi00@gmail.com>
2023-06-15 15:34:25 -0700

    betty fixed

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,18 +3,12 @@
 #include <time.h>
 #include <stdlib.h>
 
-int f1(int n)
-{
-	return ((n ^ 0x3b) & 0x3f);
-}
-int f2(long n1, int n2)
-{
-	int i, j;
-
-	for (i = 0, j = 0; i < n2; i++)
-		j += (int)*(char *)(n1 + i);
-	return ((j ^ 0x4f) & 0x3f);
-}
+/**
+ * f3 - f3
+ * @n1: n1
+ * @n2: len
+ * Return: int
+ */
 int f3(long n1, int n2)
 {
 	int i, j;
@@ -23,6 +17,13 @@ int f3(long n1, int n2)
 		j = (int)*(char *)(n1 + i) * j;
 	return ((j ^ 0x55) & 0x3f);
 }
+
+/**
+ * f4 - f4
+ * @n1: n1
+ * @n2: len
+ * Return: int
+ */
 int f4(char *n1, int n2)
 {
 	int var, i, j;
@@ -30,12 +31,19 @@ int f4(char *n1, int n2)
 	for (i = 0, j = (int)*n1; i < n2; i++)
 	{
 		if (j < (int)n1[i])
-			j =(int)n1[i];
+			j = (int)n1[i];
 	}
 	srand(j ^ 0xe);
 	var = rand();
 	return (var & 0x3f);
 }
+
+/**
+ * f5 - f5
+ * @n1: n1
+ * @n2: len
+ * Return: int
+ */
 int f5(long n1, int n2)
 {
 	int bvar;
@@ -45,6 +53,12 @@ int f5(long n1, int n2)
 		bvar = bvar + *(char *)(n1 + i) * *(char *)(n1 + i);
 	return ((bvar ^ 0xef) & 0x3f);
 }
+
+/**
+ * f6 - f6
+ * @n1: n1
+ * Return: int
+ */
 int f6(char n1)
 {
 	int i, j;
@@ -53,8 +67,16 @@ int f6(char n1)
 		j = rand();
 	return ((j ^ 0xe5) & 0x3f);
 }
+
+/**
+ * main - entry point
+ * @argc: args
+ * @argv: argv
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
+	int i, j;
 	char *s1;
 	char *args = malloc(sizeof(char) * 6);
 	int var;
@@ -65,9 +87,11 @@ int main(int argc, char *argv[])
 
 	(void) argc;
 	s1 = argv[1];
-	var = f1(strlen(s1) & 0xffffffff);
+	var = ((strlen(s1) & 0xffffffff) ^ 0x3b) & 0x3f;
 	args[0] = ((char *)local)[var];
-	var = f2((long)s1, strlen(s1));
+	for (i = 0, j = 0; i < (int)strlen(s1); i++)
+		j += (int)*(char *)((long)s1 + i);
+	var = (j ^ 0x4f) & 0x3f;
 	args[1] = ((char *)local)[var];
 	var = f3((long)s1, strlen(s1));
 	args[2] = ((char *)local)[var];
@@ -78,5 +102,5 @@ int main(int argc, char *argv[])
 	var = f6((int)*s1);
 	args[5] = ((char *)local)[var];
 	printf("%s", args);
-	return 0;
+	return (0);
 }
