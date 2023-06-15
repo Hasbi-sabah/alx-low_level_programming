b4c2efd8689f06ba6e070580cb955c37213c32f0
H. Sabah <sabahhasbi00@gmail.com>
2023-06-15 02:39:41 -0700

    null if failed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 	new_node->prev = NULL;
 	new_node->n = n;
 	if (!(*head))
