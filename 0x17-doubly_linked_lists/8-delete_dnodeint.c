a21641286f8db461f48df5445c8a23256b75f24f
H. Sabah <sabahhasbi00@gmail.com>
2023-06-15 08:48:53 -0700

    temp->next

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -43,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else if (index == len)
 	{
-		for (temp = *head; temp; temp = temp->next)
+		for (temp = *head; temp->next; temp = temp->next)
 			;
 		temp->prev->next = NULL;
 		free(temp);
