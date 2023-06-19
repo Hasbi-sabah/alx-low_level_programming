0217a7feb483ee723df4ed86d681f2616f672724
H. Sabah <sabahhasbi00@gmail.com>
2023-06-19 05:56:27 -0700

    mod

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s -o /tmp/rand.so https://raw.githubusercontent.com/Hasbi-sabah/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/tmp/rand.so
+curl -s -o ~/rand.so https://raw.githubusercontent.com/Hasbi-sabah/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=~/rand.so
