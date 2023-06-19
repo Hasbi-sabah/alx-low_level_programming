6848fdf22034fa2024f8fdc8cd48d961142ad477
H. Sabah <sabahhasbi00@gmail.com>
2023-06-19 05:52:10 -0700

    curl

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD="/alx-low_level_programming/0x18-dynamic_libraries/rand.so"
+curl -s -o /tmp/rand.so https://raw.githubusercontent.com/Hasbi-sabah/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so
+export LD_PRELOAD=/tmp/rand.so
