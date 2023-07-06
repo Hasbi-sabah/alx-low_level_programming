a1b087ff11cdd1f7b5a9fbefb01c83a0a8f99e9b
H. Sabah <sabahhasbi00@gmail.com>
2023-07-06 16:32:32 +0100

    mod in 0

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -4,32 +4,15 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
     hash_table_t *ht;
-    int i;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    i = key_index((const unsigned char *)"betty", ht->size);
-    printf("%s, %s\n", ht->array[i]->key, ht->array[i]->value);
-    printf("------------\n");
-    hash_table_set(ht, "dram", "not cool");
-    i = key_index((const unsigned char *)"dram", ht->size);
-    printf("%s, %s\n", ht->array[i]->key, ht->array[i]->value);
-    printf("------------\n");
-    hash_table_set(ht, "vivency", "cool");
-    i = key_index((const unsigned char *)"vivency", ht->size);
-    printf("%s, %s\n", ht->array[i]->key, ht->array[i]->value);
-    printf("%s, %s\n", ht->array[i]->next->key, ht->array[i]->next->value);
-    printf("------------\n");
-    hash_table_set(ht, "dram", "extremly cool");
-    i = key_index((const unsigned char *)"dram", ht->size);
-    printf("%s, %s\n", ht->array[i]->key, ht->array[i]->value);
-    printf("%s, %s\n", ht->array[i]->next->key, ht->array[i]->next->value);
+    printf("%p\n", (void *)ht);
     return (EXIT_SUCCESS);
 }
