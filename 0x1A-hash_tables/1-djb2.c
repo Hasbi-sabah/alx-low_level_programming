9e9f21b55539a02d4daf8fa49fec6b94f9058fb9
H. Sabah <sabahhasbi00@gmail.com>
2023-07-06 03:43:25 -0700

    tasks 1 2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implements the djb2 algorithm.
+ * @str: string
+ * Return: value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+	return (hash);
+}
