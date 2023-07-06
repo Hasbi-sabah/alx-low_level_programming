f633669afe307b4d65808d5fafeb98cfc63313f3
H. Sabah <sabahhasbi00@gmail.com>
2023-07-06 19:00:44 +0100

    commas again

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,22 +6,23 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	int i;
+	int i, flag;
 	hash_node_t *head;
 
 	if (!ht)
 		return;
 	printf("{");
-	for (i = 0; i < (int)ht->size; i++)
+	for (i = 0, flag = 0; i < (int)ht->size; i++)
 	{
+		if (flag && ht->array[i])
+			printf(", ");
 		for (head = ht->array[i]; head; head = head->next)
 		{
-			printf("\'%s\': \'%s\'", head->key, head->value);
+			printf("'%s': '%s'", head->key, head->value);
 			if (head->next)
 				printf(", ");
+			flag = 1;
 		}
-		if (i != (int)ht->size - 1 && ht->array[i])
-			printf(", ");
 	}
 	printf("}\n");
 }
