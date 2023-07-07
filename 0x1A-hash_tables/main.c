db69cab1acb3189fe2c890041dfb9bf75bed4e98
H. Sabah <sabahhasbi00@gmail.com>
2023-07-07 07:37:01 -0700

    cleanup

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -13,6 +13,7 @@ int main(void)
     shash_table_t *ht;
 
     ht = shash_table_create(1024);
+    shash_table_print(ht);
     shash_table_set(ht, "y", "0");
     shash_table_print(ht);
     shash_table_set(ht, "j", "1");
