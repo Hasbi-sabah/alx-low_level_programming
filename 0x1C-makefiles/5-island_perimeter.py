50acf6694176694e91a23fc1c3ed5eb9220456ba
H. Sabah <sabahhasbi00@gmail.com>
2023-08-23 19:12:57 +0000

    water AROUND the grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,22 +20,42 @@ def count_waters(grid, column, r_idx, c_idx):
     """
     counts the water around a land
     """
+    ru = 0
+    rd = 0
+    lu = 0
+    ld = 0
     water_count = 0
+    if r_idx <= 0 and c_idx <= 0:
+        water_count += 2
+        lu = 1
+    if r_idx >= len(grid) - 1 and c_idx >= len(grid[r_idx]) - 1:
+        water_count += 2
+        rd = 1
+    if c_idx <= 0 and r_idx >= len(grid) - 1:
+        water_count += 2
+        ld = 1
+    if r_idx <= 0 and c_idx >= len(grid[r_idx]) - 1:
+        water_count += 2
+        ru = 1
 
     """up is r_idx - 1, same c_idx"""
-    if grid[r_idx - 1][c_idx] == 0:
+    if (lu == 0 and ru == 0) and (r_idx <= 0 or grid[r_idx - 1][c_idx] == 0):
         water_count += 1
 
     """down r_idx + 1, same c_idx"""
-    if grid[r_idx + 1][c_idx] == 0:
+    if (ld == 0 and rd == 0) and (
+        r_idx >= len(grid) - 1 or grid[r_idx + 1][c_idx] == 0
+    ):
         water_count += 1
 
     """right same r_idx, c_idx + 1"""
-    if grid[r_idx][c_idx + 1] == 0:
+    if (rd == 0 and ru == 0) and (
+        c_idx >= len(grid[r_idx]) - 1 or grid[r_idx][c_idx + 1] == 0
+    ):
         water_count += 1
 
     """left same r_idx, c_idx - 1"""
-    if grid[r_idx][c_idx - 1] == 0:
+    if (ld == 0 and lu == 0) and (c_idx <= 0 or grid[r_idx][c_idx - 1] == 0):
         water_count += 1
 
     return water_count
