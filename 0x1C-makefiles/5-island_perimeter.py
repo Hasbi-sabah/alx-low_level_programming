dd7e863c9e6d3342f4dcdb77c1c9530907a5c7c9
H. Sabah <sabahhasbi00@gmail.com>
2023-08-23 16:09:02 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,30 @@
 #\!/usr/bin/python3
+def island_perimeter(grid):
+    perimiter = 0
+    for r_idx, row in enumerate(grid):
+        for c_idx, column in enumerate(row):
+            if column == 1:
+                perimiter += count_waters(grid, column, r_idx, c_idx)
+    return perimiter
+
+
+def count_waters(grid, column, r_idx, c_idx):
+    water_count = 0
+
+    """up is r_idx - 1, same c_idx"""
+    if grid[r_idx - 1][c_idx] == 0:
+        water_count += 1
+
+    """down r_idx + 1, same c_idx"""
+    if grid[r_idx + 1][c_idx] == 0:
+        water_count += 1
+
+    """right same r_idx, c_idx + 1"""
+    if grid[r_idx][c_idx + 1] == 0:
+        water_count += 1
+
+    """left same r_idx, c_idx - 1"""
+    if grid[r_idx][c_idx - 1] == 0:
+        water_count += 1
+
+    return water_count
