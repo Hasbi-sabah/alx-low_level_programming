b6a91d8822ad0b2cefb88b9223155bb81c3604dc
H. Sabah <sabahhasbi00@gmail.com>
2023-10-09 15:50:29 +0000

    Files added: task 11

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * using the Binary search algorith
+ *
+ * @array: array
+ * @size: size
+ * @value: value
+ *
+ * Return: index if found and -1 if not
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+	return (advanced_binary_helper(array, 0, size - 1, value));
+}
+
+/**
+ * advanced_binary_helper - recursive function
+ *
+ * @array: array
+ * @min: min
+ * @max: max
+ * @value: value
+ *
+ * Return: index if found and -1 if not
+ */
+int advanced_binary_helper(int *array, int min, int max, int value)
+{
+	int mid = (max + min) / 2;
+	int i;
+
+	if (min <= max)
+	{
+		printf("Searching in array: ");
+		for (i = min; i <= max; i++)
+		{
+			printf("%d", array[i]);
+			if (i < max)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		if (array[mid] > value)
+			return (advanced_binary_helper(array, 0, mid - 1, value));
+		else if (array[mid] < value)
+			return (advanced_binary_helper(array, mid + 1, max, value));
+		else if (array[mid] == value && array[mid - 1] == value)
+			return (advanced_binary_helper(array, 0, mid - 1, value));
+		else if (array[mid] == value)
+			return (mid);
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolation_helper(int *array, int min, int max, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_helper(int *array, int min, int max, int value);
 
 #endif
