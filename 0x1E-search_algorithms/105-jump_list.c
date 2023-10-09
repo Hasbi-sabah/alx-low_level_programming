a972f62722a98ea14cd68dee0d03bc461687f324
H. Sabah <sabahhasbi00@gmail.com>
2023-10-09 19:31:00 +0000

    Files added: task 12

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - searches for a value in a sorted linked list of integers
+ * using the Jump search algorithm
+ *
+ * @list: linked list
+ * @size: size
+ * @value: value
+ *
+ * Return: head if found and NULL if not
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	int i, j, jump = sqrt(size), flag = 0;
+	listint_t *head, *prev;
+
+	if (!list)
+		return (NULL);
+	for (i = jump, prev = list, head = list; head->next; i += jump)
+	{
+		for (j = head->index;
+			j < (int)size && head->next && (int)head->index < i;
+			j++, head = head->next)
+			;
+		printf("Value checked array[%ld] = [%d]\n", head->index, head->n);
+		if ((head->n > value || i >= (int)size) && !flag)
+		{
+			if (i >= (int)size)
+				i = size - 1;
+			printf("Value found between indexes [%ld] and [%d]\n", prev->index, i);
+			size = i + 1;
+			head = prev;
+			i = prev->index - 1;
+			jump = 1;
+			flag++;
+		}
+		else if (head->n == value)
+			return (head);
+		prev = head;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,4 +15,23 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_helper(int *array, int min, int max, int value);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 #endif
